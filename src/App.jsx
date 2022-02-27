9ec43c2554d5ba7e2237346dbf57f6da5c11c9c6
Sourabh Kheraliya <55895224+SK02K1@users.noreply.github.com>
2022-02-27 20:40:54 +0530

    add: cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Cart } from "./components/Cart";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Context Practice</h1>
+      <Cart />
     </div>
   );
 }
---
+++ src/components/Cart.jsx
@@ -0,0 +1,8 @@
+export const Cart = () => {
+  return (
+    <div className="cart">
+      <h2>Cart</h2>
+      <p>no. of items in cart: 0 </p>
+    </div>
+  );
+};
